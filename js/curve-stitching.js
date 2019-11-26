84c073ae7f92e40ee9e1f5450e90bb2046e2bb58
Shefali Nayak <nayakshefali@gmail.com>
2019-11-26 08:40:59 -0500

    improve curve stitching and added to index

--- js/curve-stitching.js
+++ js/curve-stitching.js
@@ -3,8 +3,11 @@ function paperSketch(parent) {
   var ps = parent.paperscope;
 
   var drawing = new ps.Group();
-  var x = 370;
-  var y = 230;
+  var x = parent.w/2;
+  var y = parent.h/2;
+
+  var seedGenerator = new Math.seedrandom('plotplotplot');
+  var randSeed = seedGenerator.quick();
 
   var drawFunctions = {
     parabolasInPolygon: function([numSides,numStepsPerSide]) {
@@ -12,7 +15,7 @@ function paperSketch(parent) {
       var n = numStepsPerSide;
       var p = 2 * Math.PI / v;
 
-      for (var b = 0; b <= 2 * Math.PI; b += p) {
+      for (var b = 0; b < 2 * Math.PI; b += p) {
         var g = b + p;
         var k = b + 2 * p;
         for (var h = y/n; h <= y+1; h += y/n) {
@@ -25,6 +28,7 @@ function paperSketch(parent) {
           var ptFrom = new ps.Point(x+a,y-d);
           var ptTo = new ps.Point(x+c,y-f);
           var line = ps.Path.Line(ptFrom,ptTo);
+
           drawing.addChild(line);
         }
       }
@@ -37,7 +41,7 @@ function paperSketch(parent) {
 
       for (var b = 0; b < 2 * Math.PI; b += p) {
         var g = b + p;
-        for (var h = y/n; h <= y+1; h += y/n) {
+        for (var h = y/n; h < y+1; h += y/n) {
           var t = y-h;
           var ptFrom = new ps.Point(x + t * Math.sin(b), y - t * Math.cos(b));
           var ptTo = new ps.Point(x + h * Math.sin(g), y - h * Math.cos(g));
@@ -56,8 +60,8 @@ function paperSketch(parent) {
       var circle = new ps.Path.Circle(new ps.Point(x,y),y);
       drawing.addChild(circle);
 
-      for (var b = 0; b <= 2 * Math.PI; b += p) {
-        for (var h = b; h <= p + b; h += 2 * Math.PI / n) {
+      for (var b = 0; b < 2 * Math.PI; b += p) {
+        for (var h = b; h < p + b; h += 2 * Math.PI / n) {
           var t = y * (h - b) / p;
 
           var ptFrom = new ps.Point(x + t * Math.sin(b), y - t * Math.cos(b));
@@ -72,7 +76,7 @@ function paperSketch(parent) {
     eightParabolasInSquare: function([numSteps]) {
       var v = numSteps;
 
-      for (var h = 0; h <= y+1; h += y/v) {
+      for (var h = 0; h < y+1; h += y/v) {
         for (var t = -1; t <= 1; t += 2) {
           for (var g = -1; g <= 1; g += 2) {
             for (var r = 0; r <= 1; r += 1) {
@@ -85,6 +89,37 @@ function paperSketch(parent) {
           }
         }
       }
+    },
+
+    parabolaDesign: function([numSteps]) {
+      var v = numSteps;
+
+      var rng = new Math.seedrandom(randSeed);
+
+      var j = rng() * x;
+      var k = rng() * y;
+      var l = rng() * x + x;
+      var m = rng() * y;
+      var p = rng() * x;
+      var q = rng() * y + y;
+      var r = rng() * x + x;
+      var s = rng() * y + y;
+
+      for (var n = 0; n < 1.001; n += 1/v) {
+        var ptFrom = new ps.Point(l - n*(l-j),m - n*(m-k));
+
+        var a = n * (r-p);
+        var b = n * (s-q);
+        var ptTo = new ps.Point(p+a,q+b);
+
+        var line = new ps.Path.Line(ptFrom,ptTo);
+        drawing.addChild(line);
+      }
+
+      var line = new ps.Path.Line(new ps.Point(j,k),new ps.Point(l,m));
+      drawing.addChild(line);
+      line = new ps.Path.Line(new ps.Point(p,q),new ps.Point(r,s));
+      drawing.addChild(line);
     },
 
     ellipseFromInverse: function([width,height]) {
@@ -109,8 +144,15 @@ function paperSketch(parent) {
     },
   }
 
+  var prevFunctionName;
   function update() {
     var designData = parent.data;
+
+    // on design change, reset random number generator
+    if (designData.name != prevFunctionName) {
+      randSeed = seedGenerator.quick();
+    }
+    prevFunctionName = designData.name;
 
     drawing.remove();
     drawing = new ps.Group();
@@ -120,9 +162,10 @@ function paperSketch(parent) {
     }
 
     drawing.style = {
-      strokeColor: 'palevioletred',
-      strokeWidth: 0.5
+      strokeColor: 'mediumpurple',
+      strokeWidth: 0.5,
     };
+
   }
 
   update();
